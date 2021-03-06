67c1502585b4da75541146f29c8da8a588670d7d
beeesssaaa <13708919+beeesssaaa@users.noreply.github.com>
2021-03-07 00:21:09 +0900

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Header />
+        {/* item -> snapping */}
+        {/* item -> snapping */}
+        {/* item -> snapping */}
+        {/* item -> snapping */}
     </div>
   );
 }
